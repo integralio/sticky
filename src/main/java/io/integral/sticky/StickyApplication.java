aad32421425d16f42f11cd179091ed36d7f3aaf4
Steve Anderson <steve@integral.io>
2018-09-18 10:53:08 -0400

    initial commit
    Signed-off-by: Xing Zhou <xing@integral.io>

---
+++ src/main/java/io/integral/sticky/StickyApplication.java
@@ -0,0 +1,12 @@
+package io.integral.sticky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StickyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StickyApplication.class, args);
+    }
+}
